c8f091af26df9a0399ca4c80e2c4141efe1200a7
lannn <nguyenngoclan.clc@gmail.com>
2014-04-16 18:47:11 +0700

    Added creating user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,25 @@
 class Admin::UsersController < Admin::BaseController
   def index
+    @users = User.all(order: "email")
+  end
+
+  def new
+   @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "User has been created."
+      redirect_to admin_users_path
+    else
+      flash[:alert] = "User has not been created."
+      render action: "new"
+    end
+  end
+
+  private
+  def user_params
+    params.required(:user).permit(:email, :password)
   end
 end
