6c3c6d5f61dbad576704bbc47460aa24fcdc6f86
lannn <nguyenngoclan.clc@gmail.com>
2014-04-17 17:05:13 +0700

    added create, update, delete tickets permission

--- app/helpers/admin/permissions_helper.rb
+++ app/helpers/admin/permissions_helper.rb
@@ -1,7 +1,10 @@
 module Admin::PermissionsHelper
   def permissions
     {
-      "view" => "View"
+      "view" => "View",
+      "create tickets" => "Create Tickets",
+      "update tickets" => "Update Tickets",
+      "delete tickets" => "Delete Tickets"
     }
   end
 end
