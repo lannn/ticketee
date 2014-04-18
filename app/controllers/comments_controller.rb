e62b7aad15723fec0ea04ef661005c29e9b311d0
lannn <nguyenngoclan.clc@gmail.com>
2014-04-18 18:55:42 +0700

    Update state for comment and ticket

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,7 @@ class CommentsController < ApplicationController
       flash[:notice] = "Comment has been created."
       redirect_to [@ticket.project, @ticket]
     else
+      @states = State.all
       flash[:notice] = "Comment has not been created."
       render "tickets/show"
     end 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140418090754) do
+ActiveRecord::Schema.define(version: 20140418112109) do
 
   create_table "assets", force: true do |t|
     t.string   "asset_file_name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140418090754) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "state_id"
   end
 
   create_table "permissions", force: true do |t|
@@ -46,6 +47,12 @@ ActiveRecord::Schema.define(version: 20140418090754) do
     t.datetime "updated_at"
   end
 
+  create_table "states", force: true do |t|
+    t.string "name"
+    t.string "color"
+    t.string "background"
+  end
+
   create_table "tickets", force: true do |t|
     t.string   "title"
     t.string   "description"
@@ -53,9 +60,11 @@ ActiveRecord::Schema.define(version: 20140418090754) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "state_id"
   end
 
   add_index "tickets", ["project_id"], name: "index_tickets_on_project_id"
+  add_index "tickets", ["state_id"], name: "index_tickets_on_state_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
