8f4882a1546084584862bcfbabd9161f99d24d3d
lannn <nguyenngoclan.clc@gmail.com>
2014-04-18 21:46:46 +0700

    Now admin can add a new state

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :find_states
 
   private
   def authorize_admin!
@@ -10,5 +11,9 @@ class ApplicationController < ActionController::Base
       flash[:alert] = "You must be admin to do that."
       redirect_to root_path
     end
+  end
+
+  def find_states
+    @states = State.all
   end
 end
