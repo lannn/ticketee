0af81e3d0547f19ab0e76f42df81d97f12f2aa80
lannn <nguyenngoclan.clc@gmail.com>
2014-04-21 12:08:35 +0700

    Add creating a project API

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -2,4 +2,19 @@ class Api::V1::ProjectsController < Api::V1::BaseController
   def index
     respond_with(Project.for(current_user))
   end
+
+  def create
+    project = Project.create(project_params)
+    if project.valid?
+      respond_with(project, :location => api_v1_project_path(project))
+    else
+      respond_with(project)
+    end
+  end
+
+  private
+
+  def project_params
+    params.required(:project).permit(:name)
+  end
 end
